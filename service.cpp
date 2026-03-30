ef90c492d3f091fd2b67643cb2f6e43406072f72
voldmit <toyfriend419@gmail.com>
2026-03-30 12:08:33 +0300

    Delete service.cpp

--- service.cpp
+++
@@ -1,8 +0,0 @@
-#include "service.h"
-
-Service::Service(const std::string& name, double price)
-    : name_(name), price_(price) {
-}
-
-std::string Service::GetName() const { return name_; }
-double Service::GetPrice() const { return price_; }
