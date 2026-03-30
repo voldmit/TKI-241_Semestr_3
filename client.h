65c17b2e1c5fe9387c567510eeb1f48aeffad9e8
voldmit <toyfriend419@gmail.com>
2026-03-30 22:47:55 +0300

    Update client.h

--- client.h
+++ client.h
@@ -1,23 +1,28 @@
-#pragma once
+#ifndef CLIENT_H
+#define CLIENT_H
+
 #include <string>
 #include <vector>
+#include "Person.h"
 
-class Case; 
+class Case;
 
-class Client {
+class Client : public Person {
 private:
-    int id;
-    std::string name;
     std::string contactInfo;
     std::vector<const Case*> cases;
 
 public:
     Client(int id, const std::string& name, const std::string& contactInfo);
 
-    int getId() const;
-    std::string getName() const;
     std::string getContactInfo() const;
-
+    void setContactInfo(const std::string& info);
+    
     void addCase(const Case* caseObj);
     std::vector<const Case*> getCases() const;
+    
+    // Реализация виртуального метода
+    std::string getInfo() const override;
 };
+
+#endif
