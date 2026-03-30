9a6c9fcee411b598a5cb3879455f6f418fdb1866
voldmit <toyfriend419@gmail.com>
2026-03-30 22:47:24 +0300

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,28 +1,32 @@
-﻿#include "client.h"
+#include "client.h"
 #include "case.h"
+#include <sstream>
 
 Client::Client(int id, const std::string& name, const std::string& contactInfo)
-    : id(id), name(name), contactInfo(contactInfo) {
-}
-
-int Client::getId() const {
-    return id;
-}
-
-std::string Client::getName() const {
-    return name;
+    : Person(id, name), contactInfo(contactInfo) {
 }
 
 std::string Client::getContactInfo() const {
     return contactInfo;
 }
 
+void Client::setContactInfo(const std::string& info) {
+    contactInfo = info;
+}
+
 void Client::addCase(const Case* caseObj) {
-    if (caseObj) { 
-        cases.push_back(caseObj); 
+    if (caseObj) {
+        cases.push_back(caseObj);
     }
 }
 
 std::vector<const Case*> Client::getCases() const {
-    return cases; 
+    return cases;
+}
+
+std::string Client::getInfo() const {
+    std::ostringstream oss;
+    oss << "Клиент ID: " << id << ", Имя: " << name 
+        << ", Контакты: " << contactInfo;
+    return oss.str();
 }
