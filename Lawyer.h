3fdfe20c081c5778a674947ef8c31dbc94e9cee4
voldmit <toyfriend419@gmail.com>
2026-03-30 22:46:22 +0300

    Update Lawyer.h

--- Lawyer.h
+++ Lawyer.h
@@ -1,30 +1,33 @@
 #ifndef LAWYER_H
 #define LAWYER_H
-#pragma once
+
 #include <string>
 #include <vector>
+#include "Person.h"
 #include "service.h"
 
-class Case; 
+class Case;
 
-class Lawyer {
+class Lawyer : public Person {
 private:
-    int id;
-    std::string name;
     Service specialization;
     bool isAvailable;
-    std::vector<const Case*> cases; 
+    std::vector<const Case*> cases;
 
 public:
     Lawyer(int id, const std::string& name, const Service& specialization);
 
-    int getId() const;
-    std::string getName() const;
     Service getSpecialization() const;
+    void setSpecialization(const Service& spec);
+    
     bool getAvailability() const;
-
     void setAvailability(bool available);
+    
     void addCase(const Case* caseObj);
     std::vector<const Case*> getCases() const;
+    
+    // Реализация виртуального метода
+    std::string getInfo() const override;
 };
+
 #endif
